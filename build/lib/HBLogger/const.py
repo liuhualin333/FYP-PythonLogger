e2f568fb165e6428ccad9db79f2901ac18a3fe47
Liu Hualin <liuhualin333@gmail.com>
2017-09-09 18:14:03 +0800

    delete unnecessary files and put gitignore

--- build/lib/HBLogger/const.py
+++
@@ -1,6 +0,0 @@
-PRESSING_COMPENSATION = 0.42 # Answer to all the questions in universe divided by 100
-IDLE_THRESHOLD = 1
-SPECIAL_KEYS_NAME = ["Ctrl", "Alt", "Win Key", "Shift", "Tab", "Enter", "Capital", "Back", "Up", "Down", "Left", "Right", "Insert", "Escape", "Delete", "Prior", "Next", "Home", "End", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "Space"]
-DIGRAPH_LIST = ["bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr", "ai", "au", "aw", "ay", "ea", "ee", "ei", "eu", "ew", "ey", "ie", "oi", "oo", "ou", "ow", "oy"]
-TRIGRAPH_LIST = ["nth", "sch", "scr", "shr", "spl", "spr", "squ", "str", "thr"]
-MOUSE_IDLE_THRESHOLD = 0.3
