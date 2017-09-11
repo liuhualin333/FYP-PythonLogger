beab5408ff955947b9a88bae8492c90e0079e15b
HuaBa <liuhualin333@gmail.com>
2017-09-11 23:45:37 +0800

    Fix the issue of keyboard interrupt

--- HBLogger/sniffer_Mac.py
+++ HBLogger/sniffer_Mac.py
@@ -1,4 +1,7 @@
 import sys
+import time
+import const
+from collections import deque
 from pynput import keyboard
 from pynput import mouse
 
@@ -12,6 +15,23 @@ class Sniffer:
         self.key_idle_hook = lambda x: True
         self.mouse_idle_hook = lambda x: True
         self.ditrigraph_hook = lambda x: True
+
+        #mouse/keyboard variables initialization
+        self.keyboard_last_idle_time = time.time()
+        self.keyboard_last_key = ["",0,time.time()] # [KEY_STATE, ASCII_CODE, TIME]
+        self.mouse_last_idle_time = time.time()
+        self.mouse_path_start_time = time.time()
+        self.mouse_path_end_time = time.time()
+        self.pressing = False
+        self.mouse_path = [] # A list of coordinates of mouse for one movement
+        self.digraph_found = False # Flag for finding the digraph
+        self.trigraph_found = False # Flag for finding the trigraph
+        self.keyseq = deque(["", "", ""]) # Record the last key sequence
+
+    def UpdateLastKey(self, keyVal):
+        self.keyboard_last_key[0] = keyVal
+        self.keyboard_last_key[1] = event.Ascii
+        self.keyboard_last_key[2] = time.time()
 
     def on_move(self, x, y):
         print('Pointer moved to {0}'.format(
@@ -27,12 +47,14 @@ class Sniffer:
             'down' if dy < 0 else 'up',
             (x, y)))
 
+    def on_release(self, key):
+      print('{0} release'.format(
+        key))
+
     def on_keypress(self, key):
         try:
             print('alphanumeric key {0} pressed'.format(
                 key.char))
-            if key == keyboard.Key.esc:
-                return False
 
         except AttributeError:
             print('special key {0} pressed'.format(
@@ -41,20 +63,23 @@ class Sniffer:
     def run(self):
 
         # Collect events until released
-        with mouse.Listener(
-                    on_move=self.on_move,
-                    on_click=self.on_click,
-                    on_scroll=self.on_scroll) as listener, \
-                keyboard.Listener(
-                    on_press=self.on_keypress) as listener1:
-            try:
-                while True:
-                    listener.join(100)
-                    listener1.join(100)
-            except:
-                listener.stop()
-                listener1.stop()
-                del listener
-                del listener1
+        self.listener = mouse.Listener(
+                on_move=self.on_move,
+                on_click=self.on_click,
+                on_scroll=self.on_scroll)
+        self.listener1 = keyboard.Listener(
+                on_press=self.on_keypress,
+                on_release=self.on_release)
+        self.listener.start()
+        self.listener1.start()
+        self.listener.wait()
+        self.listener1.wait()
+        while True:
+            self.listener.join(100)
+            self.listener1.join(100)
+        
     def cancel(self):
-        pass
+        self.listener.stop()
+        self.listener1.stop()
+        del self.listener
+        del self.listener1
